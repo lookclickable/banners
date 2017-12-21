af8b1110422f49b4284f939a1c50ca553cb8ed2f
Look <look@clickable.design>
2017-12-21 12:03:55 +0200

    full3

--- fonbet/full3/index.js
+++ fonbet/full3/index.js
@@ -72,25 +72,13 @@ lib.tfontAvailable = function(family, totalTypekitCount) {
 (lib.bars = function() {
 	this.initialize(img.bars);
 }).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,358,95);
-
-
-(lib.basket = function() {
-	this.initialize(img.basket);
-}).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,300,298);
+p.nominalBounds = new cjs.Rectangle(0,0,399,95);
 
 
 (lib.button = function() {
 	this.initialize(img.button);
 }).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,258,67);
-
-
-(lib.kov = function() {
-	this.initialize(img.kov);
-}).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,250,255);
+p.nominalBounds = new cjs.Rectangle(0,0,321,88);
 
 
 (lib.logo = function() {
@@ -99,28 +87,22 @@ p.nominalBounds = new cjs.Rectangle(0,0,250,255);
 p.nominalBounds = new cjs.Rectangle(0,0,250,65);
 
 
+(lib.players = function() {
+	this.initialize(img.players);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,300,362);
+
+
 (lib.shield = function() {
 	this.initialize(img.shield);
 }).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,397,119);
-
-
-(lib.smolov = function() {
-	this.initialize(img.smolov);
-}).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,521,300);
-
-
-(lib.txt = function() {
-	this.initialize(img.txt);
-}).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,300,106);
+p.nominalBounds = new cjs.Rectangle(0,0,627,128);
 
 
 (lib.wallet = function() {
 	this.initialize(img.wallet);
 }).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,393,119);// helper functions:
+p.nominalBounds = new cjs.Rectangle(0,0,549,119);// helper functions:
 
 function mc_symbol_clone() {
 	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
@@ -139,6 +121,32 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	}
 
 
+(lib.Symbol10 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.lf(["rgba(171,34,17,0)","#AB2211","#AB2211","rgba(171,34,17,0)"],[0,0.259,0.753,1],-330.3,0,330.4,0).s().p("EgznAFeIAAq7MBnPAAAIAAK7g");
+	this.shape.setTransform(330.4,35);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol10, new cjs.Rectangle(0,0,660.8,70.1), null);
+
+
+(lib.Symbol9 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#FFFFFF").s().p("AXgCWIAAgrIieAAIAAArIg2AAIAAhhIAbAAIAKgRQAHgOAEgSQAGgUAFgWQAEgbABgdIACgzICmAAIAADGIAiAAIAABhgAWCgxQgHAtgEARQgJAfgHAJIBSAAIAAiQIgzAAgAKrBsQgNgBgMgFQgMgEgLgIQgKgGgJgJQgJgHgIgLIgLgWQgFgNgDgNQgCgNABgPQgBgNACgOIAIgYQAEgMAHgKQAHgLAJgJQAIgJALgGQALgHALgFIAbgHQAOgDAPAAQANAAAOADQAOABALAFQAOAEAKAIQALAGAJAHQAJAJAGALQAHAKAFAMQAGAMACAOQADANAAAPQAAAOgDAMQgCAOgGAMQgFAMgHALQgGAKgJAJIgUAPQgKAGgOAFQgLAGgOABQgOADgNAAQgPAAgOgDgAKrhZQgNAEgKALQgJAKgHAOQgFAPgBAQQABARAFAMQAHAOAKALQAJAKANAGQAOAGAPAAQASAAANgGQAOgGAJgKQAJgLAEgOQAFgMAAgRQAAgQgFgPQgFgOgLgKQgJgLgNgEQgOgGgPAAQgRAAgMAGgA6tBsQgNgBgMgFQgNgEgKgIQgKgGgKgJQgIgJgHgKQgHgJgFgMQgEgNgDgNQgBgNgBgPQABgNABgOQACgMAEgMQAFgMAFgKQAGgLAKgJQAHgJALgGQAKgHAMgFIAbgHQAPgDAPAAQASAAAdAHQATAIAMAMIgaAqQgNgIgJgDQgOgEgQAAQgOAAgNAEQgMAGgLAJQgIALgHANQgFAPAAASQAAARAFAMQAHAQAIAKQALAJAPAGQANAGARAAQAPAAANgEQAPgFAMgJIAWAqQgFAIgJAEQgJAGgLADQgfAJgQAAQgPAAgOgDgAN9BrIAAj8ICXAAIAAA2IhfAAIAAApQALgDASAAQASAAAPAGQAPAEAMALQAKAKAHAPQAFANAAASQABAUgIAQQgGAPgMALQgOAMgSAEQgRAGgZAAIhEgBgAO1ADIAAAyIAVABQATAAAHgHQAGgGAAgMQAAgNgGgHQgIgJgOAAgAw5BrIAAj6QAqgDAhAAQAXAAAQAEQARAFAKAJQAJAJAHAKQAEAMAAAOQAAATgIAOQgHANgNAJIAAACQAFABAIAGQAGAFAFAGQAFAIAEAJQACAJAAAMQAAAUgHAPQgIAPgNAJQgMAKgSAFQgRAEgVAAQgzAAgVgBgAwAA1QAIABAMAAQAQAAAJgHQAIgGAAgOQAAgLgIgGQgHgGgQAAIgWAAgAwAhbIAAAvIAUAAQALAAAHgGQAHgGAAgOQAAgKgHgGQgIgGgMAAgAZhBoQgMgFgFgDIAUgxQAJAGAJAAQAHAAAFgDQAEgCACgGIAFgHIhui0IBCAAIBHB7IAzh7IA6AAIhWDAQgFAPgIAKQgHAMgKAIQgJAHgKADQgKAFgOAAQgLAAgKgDgARaBrIAAj8ICWAAIAAA2IhcAAIAAAtIBRAAIAAA1IhRAAIAAAuIBgAAIAAA2gAHaBrIAAjGIhYAAIAADGIg5AAIAAj8IDLAAIAAD8gACiBrIgVg1IhjAAIgVA1Ig6AAIBoj8IA0AAIBoD8gAB4AAIgchHIgdBHIA5AAgAh/BrIAAhnIhcAAIAABnIg4AAIAAj8IA4AAIAABgIBcAAIAAhgIA7AAIAAD8gAm7BrIgUg1IhjAAIgVA1Ig7AAIhKAAIhWh4IAAB4Ig4AAIAAj8IA4AAIAABuIBThuIBFAAIheB1IBmCHIBpj8IAzAAIBpD8gAnkAAIgdhHIgdBHIA6AAgAyXBrIgWg1IhjAAIgVA1Ig6AAIBpj8IAzAAIBpD8gAzCAAIgchHIgdBHIA5AAgA3oBrIAAjGIhNAAIAAg2IDSAAIAAA2IhMAAIAADGg");
+	this.shape.setTransform(181.4,15);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol9, new cjs.Rectangle(0,0,362.7,30.1), null);
+
+
 (lib.Symbol8 = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
@@ -152,51 +160,29 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 }).prototype = getMCSymbolPrototype(lib.Symbol8, new cjs.Rectangle(0,0,331.2,86.1), null);
 
 
-(lib.Symbol7 = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
-
-	// Layer 1
-	this.instance = new lib.txt();
-	this.instance.parent = this;
-	this.instance.setTransform(0,0,0.922,0.922);
-
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
-
-}).prototype = getMCSymbolPrototype(lib.Symbol7, new cjs.Rectangle(0,0,276.5,97.7), null);
-
-
 (lib.Symbol6 = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
 	// Layer 1
 	this.instance = new lib.button();
 	this.instance.parent = this;
-	this.instance.setTransform(0,0,1.365,1.365);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-}).prototype = getMCSymbolPrototype(lib.Symbol6, new cjs.Rectangle(0,0,352.2,91.5), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol6, new cjs.Rectangle(0,0,321,88), null);
 
 
 (lib.Symbol5 = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
-	// Layer 1
-	this.instance = new lib.basket();
+	// Layer 2
+	this.instance = new lib.players();
 	this.instance.parent = this;
-	this.instance.setTransform(419.5,0,1.371,1.371,9.7);
+	this.instance.setTransform(268,14);
 
-	this.instance_1 = new lib.kov();
-	this.instance_1.parent = this;
-	this.instance_1.setTransform(307,50,1.254,1.254);
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-	this.instance_2 = new lib.smolov();
-	this.instance_2.parent = this;
-	this.instance_2.setTransform(0,50,1.254,1.254);
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(1));
-
-}).prototype = getMCSymbolPrototype(lib.Symbol5, new cjs.Rectangle(0,0,825,472.4), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol5, new cjs.Rectangle(268,14,300,362), null);
 
 
 (lib.Symbol4 = function(mode,startPosition,loop) {
@@ -209,7 +195,7 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-}).prototype = getMCSymbolPrototype(lib.Symbol4, new cjs.Rectangle(0,0,533.6,161.6), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol4, new cjs.Rectangle(0,0,745.4,161.6), null);
 
 
 (lib.Symbol3 = function(mode,startPosition,loop) {
@@ -222,7 +208,7 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-}).prototype = getMCSymbolPrototype(lib.Symbol3, new cjs.Rectangle(0,0,537.6,161.2), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol3, new cjs.Rectangle(0,0,849,173.3), null);
 
 
 (lib.Symbol2 = function(mode,startPosition,loop) {
@@ -235,7 +221,27 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
-}).prototype = getMCSymbolPrototype(lib.Symbol2, new cjs.Rectangle(-120,0,611.1,162.2), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol2, new cjs.Rectangle(-120,0,681.1,162.2), null);
+
+
+(lib.Symbol7 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 3
+	this.instance = new lib.Symbol9();
+	this.instance.parent = this;
+	this.instance.setTransform(113,36.7,1,1,0,0,0,181.3,15);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	// Layer 2
+	this.instance_1 = new lib.Symbol10();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(113.1,35,1,1,0,0,0,330.4,35);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol7, new cjs.Rectangle(-217.3,0,660.8,70.1), null);
 
 
 (lib.Symbol1 = function(mode,startPosition,loop) {
@@ -267,7 +273,7 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(39).to({regX:188.6,x:159.2,alpha:0},15,cjs.Ease.get(1)).wait(95).to({regX:188.7,x:347.2},0).to({x:255.8,alpha:1},15,cjs.Ease.get(1)).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-7.9,28.8,522,138.5);
+p.nominalBounds = new cjs.Rectangle(-7.9,28.8,581.8,138.5);
 
 
 // stage content:
@@ -297,14 +303,14 @@ p.nominalBounds = new cjs.Rectangle(-7.9,28.8,522,138.5);
 			_this.logo.x = ((s.iw > s.ih) ? s.iw * 0.24 : s.iw * 0.5);
 			_this.logo.y = ((s.iw > s.ih) ? s.ih * 0.15 : s.ih * 0.1);
 			
-			_this.ico.x = ((s.iw > s.ih) ? s.iw * 0.73 : s.iw / 2);
+			_this.ico.x = ((s.iw > s.ih) ? s.iw * 0.71 : s.iw / 2);
 			_this.ico.y = ((s.iw > s.ih) ? s.ih * 0.45 : s.ih * 0.35);
 			
 			_this.spons.x = ((s.iw > s.ih) ? s.iw * 0.73 : s.iw / 2);
 			_this.spons.y = ((s.iw > s.ih) ? s.ih * 0.15 : s.ih * 0.21);
 			
 			_this.players.x = ((s.iw > s.ih) ? s.iw * 0.23 : s.iw / 2);
-			_this.players.y = ((s.iw > s.ih) ? s.ih * 0.92 : s.ih * 0.8);
+			_this.players.y = ((s.iw > s.ih) ? s.ih * 0.92 : s.ih * 0.88);
 			
 			_this.btn.x = ((s.iw > s.ih) ? s.iw * 0.73 : s.iw / 2);
 			_this.btn.y = ((s.iw > s.ih) ? s.ih * 0.87 : s.ih * 0.92);
@@ -312,9 +318,9 @@ p.nominalBounds = new cjs.Rectangle(-7.9,28.8,522,138.5);
 			
 			function setScale(scale) {
 				_this.logo.scaleX = _this.logo.scaleY = ((s.iw > s.ih) ? scale  : scale * 0.8);
-				_this.ico.scaleX = _this.ico.scaleY = scale * 0.8;
+				_this.ico.scaleX = _this.ico.scaleY = scale * 0.55;
 				_this.spons.scaleX = _this.spons.scaleY = ((s.iw > s.ih) ? scale  : scale * 0.8);
-				_this.players.scaleX = _this.players.scaleY = ((s.iw > s.ih) ? scale * 1.1 : scale * 0.8);
+				_this.players.scaleX = _this.players.scaleY = ((s.iw > s.ih) ? scale * 1.1 : scale * 1.2);
 				_this.btn.scaleX = _this.btn.scaleY = ((s.iw > s.ih) ? scale * 1.1 : scale * 0.8);
 			}
 			
@@ -348,14 +354,14 @@ p.nominalBounds = new cjs.Rectangle(-7.9,28.8,522,138.5);
 	// Symbol 1
 	this.ico = new lib.Symbol1();
 	this.ico.parent = this;
-	this.ico.setTransform(532.9,83.6,0.884,0.884,0,0,0,250.8,74.5);
+	this.ico.setTransform(561.5,104.3,0.884,0.884,0,0,0,283.1,98);
 
 	this.timeline.addTween(cjs.Tween.get(this.ico).wait(1));
 
 	// button.png
 	this.btn = new lib.Symbol6();
 	this.btn.parent = this;
-	this.btn.setTransform(544.1,628.3,1,1,0,0,0,176.1,45.7);
+	this.btn.setTransform(528.5,626.6,1,1,0,0,0,160.5,44);
 
 	this.timeline.addTween(cjs.Tween.get(this.btn).wait(1));
 
@@ -369,19 +375,19 @@ p.nominalBounds = new cjs.Rectangle(-7.9,28.8,522,138.5);
 	// txt.png
 	this.spons = new lib.Symbol7();
 	this.spons.parent = this;
-	this.spons.setTransform(544.2,-41.5,1,1,0,0,0,138.2,48.9);
+	this.spons.setTransform(518,-52.5,1,1,0,0,0,112,37.9);
 
 	this.timeline.addTween(cjs.Tween.get(this.spons).wait(1));
 
 	// Symbol 5
 	this.players = new lib.Symbol5();
 	this.players.parent = this;
-	this.players.setTransform(544.1,576.6,1,1,0,0,0,431.1,309.6);
+	this.players.setTransform(544.1,554.6,1,1,0,0,0,431.1,309.6);
 
 	this.timeline.addTween(cjs.Tween.get(this.players).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(625,169.6,825,953.8);
+p.nominalBounds = new cjs.Rectangle(700.7,169.6,660.8,885);
 // library properties:
 lib.properties = {
 	width: 1024,
@@ -392,13 +398,10 @@ lib.properties = {
 	webfonts: {},
 	manifest: [
 		{src:"images/bars.png", id:"bars"},
-		{src:"images/basket.png", id:"basket"},
 		{src:"images/button.png", id:"button"},
-		{src:"images/kov.png", id:"kov"},
 		{src:"images/logo.png", id:"logo"},
+		{src:"images/players.png", id:"players"},
 		{src:"images/shield.png", id:"shield"},
-		{src:"images/smolov.png", id:"smolov"},
-		{src:"images/txt.png", id:"txt"},
 		{src:"images/wallet.png", id:"wallet"}
 	],
 	preloads: []
